7f1cfb68fc611e222295f1aeca97c51f584e3294
tyy63 <efb63t@gmail.com>
2024-02-18 14:58:58 +0900

    【【ヘッダー】Atlasロゴにトップページへ遷移するリンクを設置する 】　対応

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -1,38 +1,43 @@
+
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8" />
-    <!--IEブラウザ対策-->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="description" content="ページの内容を表す文章" />
-    <title></title>
-    <link rel="stylesheet" href="{{ asset('css/reset.css') }} ">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }} ">
-    <!--スマホ,タブレット対応-->
-    <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <!--サイトのアイコン指定-->
-    <link rel="icon" href="画像URL" sizes="16x16" type="image/png" />
-    <link rel="icon" href="画像URL" sizes="32x32" type="image/png" />
-    <link rel="icon" href="画像URL" sizes="48x48" type="image/png" />
-    <link rel="icon" href="画像URL" sizes="62x62" type="image/png" />
-    <!--iphoneのアプリアイコン指定-->
-    <link rel="apple-touch-icon-precomposed" href="画像のURL" />
-    <!--OGPタグ/twitterカード-->
-</head>
+    <head>
+    <meta charset="utf-8" />
+        <!--IEブラウザ対策-->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="description" content="ページの内容を表す文章" />
+        <title></title>
+        <link rel="stylesheet" href="{{ asset('css/reset.css') }} ">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }} ">
+        <!--スマホ,タブレット対応-->
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <!--サイトのアイコン指定-->
+        <link rel="icon" href="画像URL" sizes="16x16" type="image/png" />
+        <link rel="icon" href="画像URL" sizes="32x32" type="image/png" />
+        <link rel="icon" href="画像URL" sizes="48x48" type="image/png" />
+        <link rel="icon" href="画像URL" sizes="62x62" type="image/png" />
+        <!--iphoneのアプリアイコン指定-->
+        <link rel="apple-touch-icon-precomposed" href="画像のURL" />
+        <!--OGPタグ/twitterカード-->
+    </head>
 <body>
     <header>
         <div id = "head">
-        <h1><a><img src="images/logo.png"></a></h1>
+            {{-- ロゴからトップページへのリンクを記載 --}}
+        <h1><a href="/top"><img src="images/atlas.png"></a></h1>
             <div id="">
                 <div id="">
                     <p>〇〇さん<img src="images/arrow.png"></p>
-                <div>
-                <ul>
-                    <li><a href="/top">ホーム</a></li>
-                    <li><a href="/profile">プロフィール</a></li>
-                    <li><a href="/logout">ログアウト</a></li>
-                </ul>
-            </div>
+                    {{-- アコーディオンメニューを作成 --}}
+                </div>
+                <div class="accordion">
+                    <p class="nav-btn">ボタン</p>
+                    <ul class="nav-menu">
+                        <li><a href="/top">ホーム</a></li>
+                        <li><a href="/profile">プロフィール編集</a></li>
+                        <li><a href="/logout">ログアウト</a></li>
+                    </ul>
+                </div>
         </div>
     </header>
     <div id="row">
@@ -58,7 +63,7 @@
     </div>
     <footer>
     </footer>
-    <script src="JavaScriptファイルのURL"></script>
-    <script src="JavaScriptファイルのURL"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="/public/js/script.js"></script>
 </body>
 </html>
